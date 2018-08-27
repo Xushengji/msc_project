77e93adde959085ec52abf364fa66d70e9d3db1e
Xushengji <33813700+Xushengji@users.noreply.github.com>
2018-08-27 19:23:25 +0000

    Update rabbitmq_throughput_tasktype.sh

--- benchmark_scripts_tasktype/rabbitmq_throughput_tasktype.sh
+++ benchmark_scripts_tasktype/rabbitmq_throughput_tasktype.sh
@@ -1,7 +1,10 @@
 for i in $(seq 1 9)
 do
     java -jar benchmarkMOM-1.0-SNAPSHOT-jar-with-dependencies.jar --threadNum $i --taskType 0 --MQ "r" --messagesNum 1000000  --exchange test1 --routingKey t1
+    sudo rabbitmqctl purge_queue test1
     java -jar benchmarkMOM-1.0-SNAPSHOT-jar-with-dependencies.jar --threadNum $i --taskType 1 --MQ "r" --messagesNum 1000000  --exchange test1 --routingKey t1
+    sudo rabbitmqctl purge_queue test1
     java -jar benchmarkMOM-1.0-SNAPSHOT-jar-with-dependencies.jar --threadNum $i --taskType 2 --MQ "r" --messagesNum 1000000  --exchange test1 --routingKey t1
+    sudo rabbitmqctl purge_queue test1
 done
 
